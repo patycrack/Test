b3786e45ab9c18f95568a15b80df1753d8f4366f
patycrack <patricia.osuna@stwarstudio.com>
2025-04-27 16:35:13 -0700

    Call the API to get the person's details.

--- src/features/personDetails/personDetailsSaga.js
+++ src/features/personDetails/personDetailsSaga.js
@@ -13,31 +13,52 @@ import {
   fetchCreditsSuccess,
 } from "./personDetailsSlice";
 
+// Manejador de la acción fetchPersonDetails
 function* fetchPersonDetailsHandler() {
   try {
+    // Obtener el ID de la persona desde el estado
     const id = yield select(selectPersonId);
 
+    // Despachar la acción fetchCredits para obtener los créditos de la persona
     yield put(fetchCredits());
-    yield delay(loadingDelay); //for loader demo purpose
+    
+    // Simulación de un retraso para la demostración del loader
+    yield delay(loadingDelay);
+
+    // Obtener los detalles de la persona desde la API
     const person = yield call(getPersonDetails, id);
+
+    // Despachar la acción para almacenar los detalles de la persona en el estado
     yield put(fetchPersonDetailsSuccess(person));
   } catch (error) {
+    // En caso de error, despachar la acción fetchError
     yield put(fetchError());
   }
 }
 
+// Manejador de la acción fetchCredits
 function* fetchPersonCreditsHandler() {
   try {
+    // Obtener el ID de la persona desde el estado
     const id = yield select(selectPersonId);
 
+    // Obtener los créditos de la persona desde la API
     const person = yield call(getPersonCredits, id);
+
+    // Despachar la acción para almacenar los créditos en el estado
     yield put(fetchCreditsSuccess(person));
   } catch (error) {
+    // En caso de error, despachar la acción fetchError
     yield put(fetchError());
   }
 }
 
+// Función que ejecuta los watchers para las acciones de fetch
 export function* personDetailsSaga() {
+  // Ejecutar el manejador fetchPersonDetailsHandler cuando la acción fetchPersonDetails sea disparada
   yield takeLatest(fetchPersonDetails.type, fetchPersonDetailsHandler);
+  
+  // Ejecutar el manejador fetchPersonCreditsHandler cuando la acción fetchCredits sea disparada
   yield takeLatest(fetchCredits.type, fetchPersonCreditsHandler);
 }
+
