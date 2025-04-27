5f95a07abe5f95248912566ffa740c0e4236346e
patycrack <patricia.osuna@stwarstudio.com>
2025-04-27 16:29:11 -0700

    An initial state is defined with values ​​such as status, movieDetails, movieId, and credits.

--- src/features/movieDetails/movieDetailsSlice.js
+++ src/features/movieDetails/movieDetailsSlice.js
@@ -1,36 +1,37 @@
 import { createSlice } from "@reduxjs/toolkit";
 
 const movieDetailsSlice = createSlice({
-  name: "movieDetails",
+  name: "movieDetails",  // Nombre del slice para identificarlo en el store
   initialState: {
-    status: "initial",
-    movieDetails: null,
-    movieId: null,
-    credits: null,
+    status: "initial",  // Estado inicial de la carga
+    movieDetails: null,  // Detalles de la película
+    movieId: null,  // ID de la película
+    credits: null,  // Créditos de la película
   },
   reducers: {
     fetchMovieDetails: (state) => {
-      state.status = "loading";
+      state.status = "loading";  // Cambia el estado a 'loading' cuando se inicie la carga de detalles
     },
     fetchMovieDetailsSuccess: (state, { payload: movieDetails }) => {
-      state.status = "success";
-      state.movieDetails = movieDetails;
+      state.status = "success";  // Cambia el estado a 'success' cuando se obtienen los detalles de la película
+      state.movieDetails = movieDetails;  // Almacena los detalles de la película
     },
     fetchError: (state) => {
-      state.status = "error";
+      state.status = "error";  // Cambia el estado a 'error' si ocurre un problema en la carga
     },
     setMovieId: (state, { payload: id }) => {
-      state.movieId = id;
+      state.movieId = id;  // Almacena el ID de la película para su posterior consulta
     },
     fetchCredits: (state) => {
-      state.status = "loading";
+      state.status = "loading";  // Cambia el estado a 'loading' cuando se inicie la carga de créditos
     },
     fetchCreditsSuccess: (state, { payload: credits }) => {
-      state.credits = credits;
+      state.credits = credits;  // Almacena los créditos de la película una vez cargados
     },
   },
 });
 
+// Exporta las acciones generadas por createSlice para usarlas en otros componentes
 export const {
   fetchMovieDetails,
   fetchError,
@@ -40,12 +41,15 @@ export const {
   fetchCreditsSuccess,
 } = movieDetailsSlice.actions;
 
+// Selector principal para acceder al estado del slice de 'movieDetails'
 const selectMovieDetailsState = (state) => state.movieDetails;
 
+// Selectores específicos para acceder a partes del estado
 export const selectStatus = (state) => selectMovieDetailsState(state).status;
 export const selectMovieDetails = (state) =>
   selectMovieDetailsState(state).movieDetails;
 export const selectMovieId = (state) => selectMovieDetailsState(state).movieId;
 export const selectCredits = (state) => selectMovieDetailsState(state).credits;
 
+// Exporta el reducer del slice, que se usará para modificar el estado en el store
 export default movieDetailsSlice.reducer;
